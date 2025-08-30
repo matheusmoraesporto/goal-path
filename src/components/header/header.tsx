c014edbe1d5ca267a31f546fcb36989e331ec179
Matheus Porto <matheus.porto@sap.com>
2025-08-30 13:15:37 -0300

    Create header component

---
+++ src/components/header/header.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function Header() {
+  return (
+    <header>
+      <div className="header-container centered-div">
+        <h1>Goal Path</h1>
+      </div>
+    </header>
+  );
+}
