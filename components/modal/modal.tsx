03bbdf0936684d50054141b153f4c2697dc69179
Matheus Porto <matheus.porto@sap.com>
2025-09-13 13:14:22 -0300

    Create modal component

---
+++ components/modal/modal.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { forwardRef, ReactNode, useImperativeHandle, useRef } from "react";
+import "./styles.css";
+
+export type ModalHandle = {
+  showModal: () => void;
+  close: () => void;
+};
+
+type ModalProps = {
+  children: ReactNode;
+};
+
+const Modal = forwardRef<ModalHandle, ModalProps>(({ children }, ref) => {
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  useImperativeHandle(ref, () => ({
+    showModal: () => dialogRef.current?.showModal(),
+    close: () => dialogRef.current?.close(),
+  }));
+
+  return (
+    <dialog ref={dialogRef} className="modal">
+      {children}
+    </dialog>
+  );
+});
+
+Modal.displayName = "Modal";
+
+export default Modal;
